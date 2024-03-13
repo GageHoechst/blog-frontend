969e4c419e0dab76b6bcb28038ccbffac533250b
GageHoechst <hoechstg99@gmail.com>
2024-03-13 16:54:24 -0400

    added a useState hook

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable react/prop-types */
 export function PostsIndex(props) {
   console.log(props);
   return (
     <div id="posts-index">
       <h1>Monsters</h1>
       <h2>The name is {props.name}</h2>
-      {props.posts.map((post) => (
-        <div key={post.id} className="Posts">
-          <h2>{post.title}</h2>
-          <img src={post.image} />
-          <p>body:{post.body}</p>
-          <img src="" alt="" />
-        </div>
-      ))}
+      <div className="cards">
+        {props.posts.map((post) => (
+          <div key={post.id} className="Posts">
+            <h2>{post.title}</h2>
+            <img src={post.image} />
+            <p>body:{post.body}</p>
+            <img src="" alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
