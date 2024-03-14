5d8f0e5102737132eddd430906bc6b289511ddf6
GageHoechst <hoechstg99@gmail.com>
2024-03-14 15:30:52 -0400

    started adding a handle submit function

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable no-undef */
+import axios from "axios";
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/posts.json", params);
+  };
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form method="POST" action="http://localhost:3000/posts.json">
+      <form onSubmit={handleSubmit}>
         <div>
           Title: <input name="title" type="text" />
         </div>
