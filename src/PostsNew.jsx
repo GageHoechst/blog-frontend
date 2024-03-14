b0668319235435ab2d79349c2bac5a24b395c359
GageHoechst <hoechstg99@gmail.com>
2024-03-14 15:05:00 -0400

    added a post command

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -2,6 +2,18 @@ export function PostsNew() {
   return (
     <div id="posts-new">
       <h1>New post</h1>
+      <form method="POST" action="http://localhost:3000/posts.json">
+        <div>
+          Title: <input name="title" type="text" />
+        </div>
+        <div>
+          Body: <input name="Body" type="text" />
+        </div>
+        <div>
+          Image: <input name="Image" type="text" />
+        </div>
+        <button type="submit">Create Post</button>
+      </form>
     </div>
   );
 }
