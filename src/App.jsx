00ae046842af2c7a234af35055178efe9fe00092
GageHoechst <hoechstg99@gmail.com>
2024-04-04 15:06:03 -0400

    added the ability to allow web requests from the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable no-undef */
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { BrowserRouter } from "react-router-dom";
-
+import axios from "axios";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://mini-capstone-api-wbid.onrender.com/products.json";
 function App() {
   return (
     <div>
