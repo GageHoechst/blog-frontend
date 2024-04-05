15f1707769c0bfa238f805d31b4317ddd22a8c04
GageHoechst <hoechstg99@gmail.com>
2024-04-05 12:35:33 -0400

    added character limit to signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export function Signup() {
   const [errors, setErrors] = useState([]);
-
+  const [name, setName] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
     setErrors([]);
@@ -31,8 +31,9 @@ export function Signup() {
       </ul>
       <form onSubmit={handleSubmit}>
         <div>
-          Name: <input name="name" type="text" />
+          Name: <input name="name" value={name} onChange={(event) => setName(event.target.value)} type="text" />
         </div>
+        <small>{20 - name.length} characters remaining</small>
         <div>
           Email: <input name="email" type="email" />
         </div>
